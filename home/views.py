f4b46074a11b179e3b9b575cb6817495f8caf5e9
Chris Palmer <cgpalmer91@gmail.com>
2021-02-15 22:45:54 +0000

    uploading files

--- home/views.py
+++ home/views.py
@@ -1,35 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import pandas as pd
-
+from .forms import UploadFileForm
+import os.path
 # Create your views here.
 def index(request):
-    deadline = pd.read_csv('test_data.csv') 
+        # # Grouping the dates
+
+        # deadline.insert(loc=len(deadline.columns), column="Date_count", value="pending", allow_duplicates=False)
+        # deadline = deadline[deadline['Course Name']==str(input1)]
+
+        # date_analysis = list(deadline['Date'].unique())
 
 
-    # input1 = input('Enter course name: ')
-    # print("Looking for deadline data regarding " + str(input1) + ".")
+        # for date_number in range(len(date_analysis)):
+        #     deadline_by_date = deadline.loc[deadline['Date'] == date_analysis[date_number]]
+        #     return_count = deadline_by_date.count()
+        #     deadline.loc[(deadline['Date'] == date_analysis[date_number]),'Date_count']=str(return_count['Date'])
+        # deadline = deadline.sort_values(by=['Date_count', 'Date'], ascending=True)
+        # print(deadline)
+        
+        # deadline.to_csv(index=False, path_or_buf="student_version.csv")
+
+        # No user input - office version
 
 
 
-    # # Grouping the dates
+        # compression_opts = dict(method='zip',
+        #                         archive_name='out.csv')  
+        # deadline.to_csv('out.zip', index=False,
+        #           compression=compression_opts)
+        
+        # deadline.to_csv(index=False, path_or_buf="office_sorted.csv")
+        
 
-    # deadline.insert(loc=len(deadline.columns), column="Date_count", value="pending", allow_duplicates=False)
-    # deadline = deadline[deadline['Course Name']==str(input1)]
-
-    # date_analysis = list(deadline['Date'].unique())
 
 
-    # for date_number in range(len(date_analysis)):
-    #     deadline_by_date = deadline.loc[deadline['Date'] == date_analysis[date_number]]
-    #     return_count = deadline_by_date.count()
-    #     deadline.loc[(deadline['Date'] == date_analysis[date_number]),'Date_count']=str(return_count['Date'])
-    # deadline = deadline.sort_values(by=['Date_count', 'Date'], ascending=True)
-    # print(deadline)
-    
-    # deadline.to_csv(index=False, path_or_buf="student_version.csv")
+    form = UploadFileForm()
+    context = {
+      
+        'form':form
+    }
+    return render(request, 'home/index.html', context,)
 
-    # No user input - office version
+def handle_uploaded_file(f):
+    with open(f'upload_folder/{f}', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
 
+
+def upload(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            f = request.FILES['file']
+            handle_uploaded_file(f)
+            writing_result(f)
+            return redirect('home')
+    else:
+        form = UploadFileForm()
+    return redirect('home')
+
+def writing_result(f):
+    deadline = pd.read_csv(f'upload_folder/{f}') 
     deadline.insert(loc=len(deadline.columns), column="Date_count", value="pending", allow_duplicates=False)
 
     date_analysis = list(deadline['Date'].unique())
@@ -38,21 +70,9 @@ def index(request):
         deadline_by_date = deadline.loc[deadline['Date'] == date_analysis[date_number]]
         return_count = deadline_by_date.count()
         deadline.loc[(deadline['Date'] == date_analysis[date_number]),'Date_count']=str(return_count['Date'])
-    deadline = deadline.sort_values(by=['Date_count', 'Date'], ascending=False)
-    print(deadline)
-
-    # compression_opts = dict(method='zip',
-    #                         archive_name='out.csv')  
-    # deadline.to_csv('out.zip', index=False,
-    #           compression=compression_opts)
-    
-    # deadline.to_csv(index=False, path_or_buf="office_sorted.csv")
+    deadline = deadline.sort_values(by=['Date'], ascending=False)
+ 
     deadline_table = deadline.to_html(index=False)
-
     text_file = open("templates/includes/result.html", "w")
     text_file.write(deadline_table)
     text_file.close()
-    context = {
-        'deadline': deadline_table
-    }
-    return render(request, 'home/index.html', context,)
